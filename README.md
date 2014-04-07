ba5d9c6dd24e2ed09a8007816017aaefdc782569
KoryNunn <knunn187@gmail.com>
2014-04-07 14:18:30 +1000

    Added valid, changed method to be bindable, changed compatibility to gaffa >= 1.4.3

---
+++ README.md
@@ -0,0 +1,19 @@
+# gaffa-form
+
+form view for gaffa
+
+## Install:
+
+    npm i gaffa-form
+
+## Add to gaffa:
+
+    gaffa.views.constructors.form = require('gaffa-form');
+
+# API
+
+## Properties (instanceof Gaffa.Property)
+
+### valid (set)
+
+    The rendered elements validity state.
--- form.js
+++ form.js
@@ -1,34 +1,49 @@
 var Gaffa = require('gaffa'),
-    viewType = "form",
-    crel = require('crel'),
-	cachedElement;
+    doc = require('doc-js'),
+    crel = require('crel');
 
 function Form(){}
 Form = Gaffa.createSpec(Form, Gaffa.ContainerView);
-Form.prototype.type = viewType;
+Form.prototype._type = 'form';
 
 Form.prototype.render = function(){
-    var viewModel = this,
+    var view = this,
         renderedElement = crel('form')
 
     if (this.action) {
         renderedElement.setAttribute("action", this.action);
     } else {
         renderedElement.addEventListener('submit', function (event) {
-            if(viewModel.actions.submit){
+            if(view.actions.submit){
                 event.preventDefault();
             }
         });
-    }
-
-    if (this.method) {
-        renderedElement.setAttribute("method", this.method);
     }
 
     this.views.content.element = renderedElement;
 
     this.renderedElement = renderedElement;
 
+    doc(renderedElement).on('change', function(){
+        if(!renderedElement.checkValidity){
+            // This browser is old.
+            return;
+        }
+
+        // https://code.google.com/p/chromium/issues/detail?can=2&q=checkValidity&colspec=ID%20Pri%20M%20Iteration%20ReleaseBlock%20Cr%20Status%20Owner%20Summary%20OS%20Modified&id=360444&thanks=360444&ts=1396842451
+        setTimeout(function(){
+            view.valid.set(renderedElement.checkValidity());
+        },0);
+    });
+
 };
+
+Form.prototype.method = new Gaffa.Property({
+    update: function(view, value){
+        view.renderedElement.setAttribute('method', value);
+    },
+    value: 'POST'
+});
+Form.prototype.valid = new Gaffa.Property();
 
 module.exports = Form;
